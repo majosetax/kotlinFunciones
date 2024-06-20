b14f62add87f607d37dc608b4f6d8a094550165f
majosetax <mariajolopezsc@gmail.com>
2024-06-20 09:27:35 -0500

    kotlin

---
+++ src/NotificacionesMoviles.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val notificacionMañana = 51
+    val notificacionNoche = 135
+
+    escribirNotificacionSuma(notificacionMañana)
+    escribirNotificacionSuma(notificacionNoche)
+}
+
+fun escribirNotificacionSuma(numeroMensajes: Int){
+
+}
